ab65cd693e79b903e5529b5f27ef23514f2ed076
Matthias <tuttiacc@hotmail.com>
2022-06-28 19:38:23 +0200

    added shared styles, fixed some UI issues, fixed devdependency

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -3,4 +3,6 @@ export * from './elements/all-events-calendar';
 export * from './elements/calendar-events-context';
 export * from './elements/calendar-event-detail';
 export * from './calendar-events-service';
+export * from './context';
 export * from './types';
+export * from './elements/sharedStyles';
